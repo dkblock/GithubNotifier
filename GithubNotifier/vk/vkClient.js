230f9fb128648209c898978018ea32ea7109a206
dkblock <wadeouter@gmail.com>
2019-08-10 13:58:46 +0300

    Update vk client

--- GithubNotifier/vk/vkClient.js
+++ GithubNotifier/vk/vkClient.js
@@ -8,7 +8,7 @@ export function sendMessage(commit, repository) {
     const token = config.vk.token;
     const randomID = getRandomID();
     const url = `https://api.vk.com/method/messages.send?user_id=${userID}&message=${message}&random_id=${randomID}&group_id=${groupID}&dont_parse_links=1&access_token=${token}&v=5.101`;
-
+    
     fetch(url);
 }
 
