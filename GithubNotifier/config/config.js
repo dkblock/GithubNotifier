af3745e60bc486c8872b0f7b9e48229de353a38d
dkblock <wadeouter@gmail.com>
2019-08-21 18:07:01 +0300

    Update notifier, add array of receivers to config

--- GithubNotifier/config/config.js
+++ GithubNotifier/config/config.js
@@ -7,16 +7,22 @@ export const config = {
         user: settings.github.user,
         repositories: settings.github.repositories,  
         clientID: settings.github.clientID,
-        clientSecret: settings.github.clientSecret 
+        clientSecret: settings.github.clientSecret, 
     },
     slack: {
         token: settings.slack.token,
-        channel: settings.slack.channel
+        channel: settings.slack.channel,
+        client: './notificationReceivers/slack/slackClient'
     },
     vk: {
         userID: settings.vk.userID,
         groupID: settings.vk.groupID,
-        token: settings.vk.token
+        token: settings.vk.token,
+        client: './notificationReceivers/vk/vkClient'
     },
+    console: {
+        client: './notificationReceivers/console/printer'
+    },
+    notificationReceivers: settings.notificationReceivers,
     interval: 5000
 }
--- GithubNotifier/notifier.js
+++ GithubNotifier/notifier.js
@@ -1,9 +1,14 @@
-import {sendMessage as sendMessageToSlack} from './notificationReceivers/slack/slackClient';
-import {sendMessage as sendMessageToVK} from './notificationReceivers/vk/vkClient';
-import {printMessage as printMessageOnConsole} from './notificationReceivers/console/printer'
+import {config} from './config/config';
 
-export default function sendNotifications(commit, repository) {
-    sendMessageToSlack(commit, repository);
-    sendMessageToVK(commit, repository);
-    printMessageOnConsole(commit, repository);
+export default async function sendNotifications(commit, repository) {
+    const notificationReceivers = config.notificationReceivers;
+
+    notificationReceivers.forEach(receiver => {
+        sendMessage(commit, repository, receiver);
+    });
+}
+
+async function sendMessage(commit, repository, receiver) {
+    const client = await import(config[receiver].client);
+    client.sendMessage(commit, repository);
 }
